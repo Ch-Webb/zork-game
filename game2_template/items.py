749b06af6bf01bea83aaa9ed446cd70802497ea7
Charlie Webb <cjsw00@hotmail.co.uk>
2018-10-19 08:57:43 +0100

    Final Code Submission

---
+++ game2_template/items.py
@@ -0,0 +1,90 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+
+    "mass": 0.005
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!",
+
+    "mass": 2.8
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees.",
+
+    "mass": 0.05
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits.",
+
+    "mass": 0.2
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen.",
+
+    "mass": 0.01
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously.",
+
+    "mass": 0.9
+}
+
+item_key = {
+    "id": "key",
+
+    "name": "key",
+
+    "description": "This old key looks like it could fit in some sort of lock...",
+
+    "mass": 0.2
+}
+item_chest = {
+    "id": "chest",
+    "name": "a chest",
+    "description": "A small chest. It looks as if it hasnt been opened in years...",
+    "mass": 4
+    }
+
+items_all = {
+    "ID": item_id,
+    "Laptop": item_laptop,
+    "Money": item_money,
+    "Biscuits": item_biscuits,
+    "Pen": item_pen,
+    "Handbook": item_handbook,
+    "Key": item_key,
+    "Chest": item_chest
+    }
